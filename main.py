4094ea9c1d92c7a58a5a78136d14b19e0fc4f011
monman53 <monman.cs@gmail.com>
2023-02-16 23:27:20 +0900

    Minimal implement

--- main.py
+++ main.py
@@ -17,8 +17,19 @@ db.init_app(app)
 
 class Recipe(db.Model):
   id = db.Column(db.Integer, primary_key=True)
+
   name = db.Column(db.String, nullable=False)
-  added = db.Column(db.DateTime, default=datetime.datetime.now)
+  image_path = db.Column(db.String)
+  memo = db.Column(db.String)
+  created_date = db.Column(db.DateTime, default=datetime.datetime.now)
+
+  # Chomiryo
+  shoyu = db.Column(db.Integer, default=0)
+  sato = db.Column(db.Integer, default=0)
+  mirin = db.Column(db.Integer, default=0)
+  shio = db.Column(db.Integer, default=0)
+  su = db.Column(db.Integer, default=0)
+  sake = db.Column(db.Integer, default=0)
 
 with app.app_context():
   db.create_all()
@@ -27,19 +38,40 @@ with app.app_context():
 @app.route("/")
 def root():
   recipes = db.session.execute(db.select(Recipe)).scalars()
-  print(recipes)
   return render_template("index.html", recipes=recipes)
 
-@app.route("/recipes/create", methods=["POST"])
+@app.route("/recipes/<int:recipe_id>", methods=["GET"])
+def recipe(recipe_id):
+  record = db.select(Recipe).filter_by(id=recipe_id)
+  recipe = db.session.execute(record).scalar_one()
+  return render_template("recipes/recipe.html", recipe=recipe)
+
+@app.route("/recipes/create", methods=["GET", "POST"])
 def recipe_create():
-  recipe = Recipe(name=request.form["name"])
-  db.session.add(recipe)
-  db.session.commit()
-  return redirect(url_for("root"))
+  if request.method == "POST":
+    recipe = Recipe(
+      name=request.form["name"],
+      memo=request.form["memo"],
+      
+      # Chomiryo
+      shoyu=request.form["shoyu"],
+      sato=request.form["sato"],
+      mirin=request.form["mirin"],
+      shio=request.form["shio"],
+      su=request.form["su"],
+      sake=request.form["sake"],
+    )
+    db.session.add(recipe)
+    db.session.commit()
+    return redirect(url_for("root"))
+  
+  return render_template('recipes/create.html')
 
 @app.route("/recipes/delete", methods=["POST"])
 def recipe_delete():
-  recipe = db.session.execute(db.select(Recipe).filter_by(id=request.form["key"])).scalar_one()
+  id = request.form["id"]
+  record = db.select(Recipe).filter_by(id=id)
+  recipe = db.session.execute(record).scalar_one()
   db.session.delete(recipe)
   db.session.commit()
   return redirect(url_for("root"))
